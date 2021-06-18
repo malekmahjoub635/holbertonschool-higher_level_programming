969b1c6b8bed35c7f7f871215df8b398a87960b3
malekmahjoub635 <malouka99@live.com>
2021-06-18 12:14:07 +0000

    Python - Test-driven development

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module that prints a text with 2 lines after these characters (./?/:)
+"""
+
+
+def text_indentation(text):
+    """Prints text with added two newlines
+    after each of these characters {'.', '?', ':'}.
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print("{}".format(text), end="")
