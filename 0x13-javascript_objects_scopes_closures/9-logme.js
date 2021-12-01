704fed8c70b1051bd7e33688a3fa9dcaeccf69d2
malekmahjoub635 <malouka99@live.com>
2021-12-01 13:01:47 +0000

    semistandard fix

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// a function that prints the number of arguments already printed and the new argument value. 
+// a function that prints the number of arguments already printed and the new argument value.
 let argument = 0;
 exports.logMe = function (item) {
   console.log(argument + ': ' + item);
