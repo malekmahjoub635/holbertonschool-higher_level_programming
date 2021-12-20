f50b83713a66e1f0535ccc5d4373cc47bd63d4b6
malekmahjoub635 <malouka99@live.com>
2021-12-20 22:54:32 +0000

    fix

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-''' a script that takes in an argument and displays
-    all values in the states table of hbtn_0e_0_usa'''
+''' takes in an argument and displays '''
 if __name__ == "__main__":
     import MySQLdb
     import sys
 
     data = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           password=sys.argv[2], db=sys.argv[3])
+                           passwd=sys.argv[2], db=sys.argv[3])
     cur = data.cursor()
-    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id",
-                .format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE BINARY name=%s ORDER BY id ASC",
+                (sys.argv[4],))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
