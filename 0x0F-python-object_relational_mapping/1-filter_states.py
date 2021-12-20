e7d26a4f435c268e6d3a9d5148b2cf6473ea92ce
malekmahjoub635 <malouka99@live.com>
2021-12-20 22:39:19 +0000

    fixing pep8

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
                            password=sys.argv[2], db=sys.argv[3])
     cur = data.cursor()
     cur.execute("SELECT * FROM states\
-                WHERE name='{}'\
-                ORDER BY id ASC".format(sys.argv[4]))
+                WHERE name LIKE BINARY 'N%'\
+                ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
